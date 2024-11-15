421e2bd279732fa69314fbff7e7eab5141a2e912
Ville Huumo <ville.huumo@knowit.fi>
2024-11-15 13:01:45 +0200

    Test

--- command.php
+++ command.php
@@ -12,4 +12,6 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
+// Test
+
 WP_CLI::add_command('fixtures', __NAMESPACE__ . '\\Command');
